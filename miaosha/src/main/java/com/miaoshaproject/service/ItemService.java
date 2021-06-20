3f7aac23f4dfd19eca01252f6234b0a15e7f9b54
yjreset <yjreset@gmail.com>
2021-06-21 01:53:34 +0800

    商品模型完成

---
+++ miaosha/src/main/java/com/miaoshaproject/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.miaoshaproject.service;
+
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.service.model.ItemModel;
+
+import java.util.List;
+
+public interface ItemService {
+    //创建商品
+    ItemModel createItem(ItemModel itemModel) throws BusinessException;
+    List<ItemModel> listItem();
+    ItemModel getItemById(Integer id);
+}
